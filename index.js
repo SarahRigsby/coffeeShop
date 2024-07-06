931392a175148da91922902d7f2b59f91e63ebe1
SarahRigsby <sarahrigsby2019@gmail.com>
2024-07-06 16:54:22 -0500

    more js added

--- index.js
+++ index.js
@@ -5,43 +5,63 @@ const menuItems = [
     { id: 3, item: "Cappuccino", price: 3, image: 'https://github.com/bryanrigsby/imagesForCoffeeShopSite/blob/main/resized_cappuccino.jpeg?raw=true' },
     { id: 4, item: "Latte", price: 2, image: 'https://github.com/bryanrigsby/imagesForCoffeeShopSite/blob/main/resized_latte.jpeg?raw=true' },
     { id: 5, item: "Mocha", price: 4, image: 'https://github.com/bryanrigsby/imagesForCoffeeShopSite/blob/main/resized_mocha.jpeg?raw=true' },
-    { id: 6, item: "Americano", price: 2, image: 'https://github.com/bryanrigsby/imagesForCoffeeShopSite/blob/main/resized_americano.jpeg?raw=true' }
+    { id: 6, item: "Americano", price: 2, image: 'https://github.com/bryanrigsby/imagesForCoffeeShopSite/blob/main/resized_americano.jpeg?raw=true' },
 ]
 window.onload = function(){
     let menuItemElement= document.getElementById('menuItem')
     for (let index = 0; index < menuItems.length; index++) {
-        console.log('Menu Items')
-        console.log(menuItems)
+        console.log('index', index);
+        let temp = 0;
+        if (index > 0) {
+            temp = index + 2;
+        }
+        console.log('temp',temp)
+        console.log(menuItems[index])
         // menuItemElement.innerText = menuItems[index]["item"]
-        const newDivElement = document.createElement('div');
-        // make card div
-        const cardDivElement = document.createElement('div');
-        cardDivElement.className ="card";
-        cardDivElement.style.width="18rem";
-        // make image
-        const cardImgElement = document.createElement('img');
-        cardImgElement.src = menuItems[index]['image'];
-        cardImgElement.className ="card-img-top";
-        cardImgElement.alt = menuItems[index].item;
-        cardDivElement.appendChild(cardImgElement);
-        // make card body div
-        const cardBodyElement = document.createElement('div');
-        cardBodyElement.className ='card-body';
-        cardDivElement.appendChild(cardBodyElement);
-        // inside card body
-        // make card title h5
-        const cardTitleElement = document.createElement('h5');
-        cardTitleElement.className='card-title';
-        cardTitleElement.innerText = menuItems[index]["item"]+" - $"+menuItems[index]["price"];
-        cardDivElement.appendChild(cardTitleElement);
-        // make add to cart button
+        
+            const newDivElement = document.createElement('div');
+            newDivElement.className ="row";
 
-        // set inner text of newDivElement
-        // newDivElement.innerText = menuItems[index]["item"];
-        // append to newDivElement
-        newDivElement.appendChild(cardDivElement);
+            for (let j = 0; j < 3; j++) {
+                const newColElement = document.createElement('div');
+                newColElement.className ="col-12 col-md-4";
+                newDivElement.appendChild(newColElement);
+                // make card div
+                const cardDivElement = document.createElement('div');
+                cardDivElement.className ="card";
+                cardDivElement.style.width="18rem";
+                newColElement.appendChild(cardDivElement)
+                // make image
+                const cardImgElement = document.createElement('img');
+                cardImgElement.src = menuItems[temp]['image'];
+                cardImgElement.className ="card-img-top";
+                cardImgElement.alt = menuItems[temp].item;
+                cardDivElement.appendChild(cardImgElement);
+                // make card body div
+                const cardBodyElement = document.createElement('div');
+                cardBodyElement.className ='card-body';
+                cardDivElement.appendChild(cardBodyElement);
+                // inside card body
+                // make card title h5
+                const cardTitleElement = document.createElement('h5');
+                cardTitleElement.className='card-title';
+                cardTitleElement.innerText = menuItems[temp]["item"]+" - $"+menuItems[temp]["price"];
+                cardDivElement.appendChild(cardTitleElement);
+                if (temp > menuItems.length) {
+                    return
+                    
+                }
+                temp+=1
+            }
 
-        menuItemElement.appendChild(newDivElement);
+            // make column div
+            // make add to cart button
+    
+            
+    
+            menuItemElement.appendChild(newDivElement);
+
+        
     }
 }
 
