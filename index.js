b2c7153bf8746f584e52863e521f27da2f4fb220
SarahRigsby <sarahrigsby2019@gmail.com>
2024-07-07 17:11:49 -0500

    fixing bugs

--- index.js
+++ index.js
@@ -12,118 +12,80 @@ const menuItems = [
 ]
 window.onload = function(){
     console.log('cart',cart)
-    let menuItemElement= document.getElementById('menuItem')
+    let menuItemElement= document.getElementById('menuItemContainer')
 
-    //testing
     for (let index = 0; index < menuItems.length; index++) {
-        // make card div
-        const cardDivElement = document.createElement('div');
-        cardDivElement.className ="card";
-        cardDivElement.style.width="18rem";
-        menuItemElement.appendChild(cardDivElement)
-        // make image
-        const cardImgElement = document.createElement('img');
-        cardImgElement.src = menuItems[index]['image'];
-        cardImgElement.className ="card-img-top";
-        cardImgElement.alt = menuItems[index].item;
-        cardDivElement.appendChild(cardImgElement);
-        // make card body div
-        const cardBodyElement = document.createElement('div');
-        cardBodyElement.className ='card-body';
-        cardDivElement.appendChild(cardBodyElement);
-        // inside card body
-        // make card title h5
-        const cardTitleElement = document.createElement('h5');
-        cardTitleElement.className='card-title';
-        cardTitleElement.innerText = menuItems[index]["item"]+" - $"+menuItems[index]["price"];
-        const addToCart =document.createElement("button");
-        addToCart.classList="btn btn-primary";
-        addToCart.innerText = 'Add to Cart';
-        addToCart.onclick=()=>addedToCart(menuItems[index]);
-        cardTitleElement.appendChild(addToCart);
-        cardBodyElement.appendChild(cardTitleElement);
+        // console.log('index', index);
+        
+        const newDivElement = document.createElement('div');
+        newDivElement.className ="row";
+
+        for (let j = 0; j < 3; j++) {
+
+            //calculate current item index
+            let currentItemIndex = index + j;
+
+            // check if the currentItemIndex is within the bounds of menitems array
+            if (currentItemIndex >= menuItems.length) {
+                break;
+            }
+
+            const newColElement = document.createElement('div');
+            newColElement.className ="col-12 col-md-4";
+            newDivElement.appendChild(newColElement);
+
+            // create card div
+            const cardDivElement = document.createElement('div');
+            cardDivElement.className ="card";
+            cardDivElement.style.width="18rem";
+            newColElement.appendChild(cardDivElement)
+
+            // create image element
+            const cardImgElement = document.createElement('img');
+            cardImgElement.src = menuItems[currentItemIndex]['image'];
+            cardImgElement.className ="card-img-top";
+            cardImgElement.alt = menuItems[currentItemIndex].item;
+            cardDivElement.appendChild(cardImgElement);
+
+            // create card body div
+            const cardBodyElement = document.createElement('div');
+            cardBodyElement.className ='card-body';
+            cardDivElement.appendChild(cardBodyElement);
+
+            // inside card body
+            // create card title h5
+            const cardTitleElement = document.createElement('h5');
+            cardTitleElement.className='card-title';
+            cardTitleElement.innerText = menuItems[currentItemIndex]["item"]+" - $"+menuItems[currentItemIndex]["price"];
+            cardBodyElement.appendChild(cardTitleElement);
+
+            // create add to cart button
+            const addToCart =document.createElement("button");
+            addToCart.classList="btn btn-primary";
+            addToCart.innerText = 'Add to Cart';
+            addToCart.onclick=()=>addedToCart(menuItems[currentItemIndex]);
+            cardBodyElement.appendChild(addToCart);
+        }
+
+        menuItemElement.appendChild(newDivElement);
+
+        // Increment index to skip already processed items
+        index += 2; // Skip next 2 items as they are already processed in the inner loop
+
     }
-// display cart
-let cartElement= document.getElementById('cart');
-   for (let i = 0; i < cart.length; i++) {
-      console.log('cart at index i', cart[i]);
-   }     
 }
 
-    //end testing
-
-
-
-    // for (let index = 0; index < menuItems.length; index++) {
-    //     console.log('index', index);
-    //     let temp = 0;
-    //     if (index > 0) {
-    //         temp = index + 2;
-    //     }
-    //     console.log('temp',temp)
-    //     console.log(menuItems[index])
-    //     // menuItemElement.innerText = menuItems[index]["item"]
-        
-    //         const newDivElement = document.createElement('div');
-    //         newDivElement.className ="row";
-
-    //         for (let j = 0; j < 3; j++) {
-    //             const newColElement = document.createElement('div');
-    //             newColElement.className ="col-12 col-md-4";
-    //             newDivElement.appendChild(newColElement);
-    //             // make card div
-    //             const cardDivElement = document.createElement('div');
-    //             cardDivElement.className ="card";
-    //             cardDivElement.style.width="18rem";
-    //             newColElement.appendChild(cardDivElement)
-    //             // make image
-    //             const cardImgElement = document.createElement('img');
-    //             cardImgElement.src = menuItems[temp]['image'];
-    //             cardImgElement.className ="card-img-top";
-    //             cardImgElement.alt = menuItems[temp].item;
-    //             cardDivElement.appendChild(cardImgElement);
-    //             // make card body div
-    //             const cardBodyElement = document.createElement('div');
-    //             cardBodyElement.className ='card-body';
-    //             cardDivElement.appendChild(cardBodyElement);
-    //             // inside card body
-    //             // make card title h5
-    //             const cardTitleElement = document.createElement('h5');
-    //             cardTitleElement.className='card-title';
-    //             cardTitleElement.innerText = menuItems[temp]["item"]+" - $"+menuItems[temp]["price"];
-    //             const addToCart =document.createElement("button");
-    //             addToCart.classList="btn btn-primary";
-    //             addToCart.innerText = 'Add to Cart';
-    //             addToCart.onclick=()=>addedToCart(temp);
-    //             cardTitleElement.appendChild(addToCart);
-    //             cardBodyElement.appendChild(cardTitleElement);
-    //             if (temp > menuItems.length) {
-    //                 return
-                    
-    //             }
-    //             temp+=1
-    //         }
-
-    //         // make column div
-    //         // make add to cart button
-    
-            
-    
-    //         menuItemElement.appendChild(newDivElement);
-
-        
-    // }
-// }
-
 function addedToCart(item){
-  console.log('addToCart', item);
+//   console.log('addToCart', item);
   cart.push(item);
   displayCart();
-  console.log('cart',cart);
+//   console.log('cart',cart);
 }
 
 function displayCart(){
     let cartElement= document.getElementById('cart');
+    cartElement.replaceChildren();
+    console.log('cartElement', cartElement);
     for (let i = 0; i < cart.length; i++) {
         console.log('cart at index i', cart[i]);
         const cartItemElement = document.createElement('div');
